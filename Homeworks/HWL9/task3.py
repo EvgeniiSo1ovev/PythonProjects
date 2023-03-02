25a79906c828669291545cb2addd3858628f7220
Evgenii Solovev <krekersol@mail.ru>
2023-03-02 19:41:27 +0900

    Добавлено решение задания про метакласс семинара 9

---
+++ Homeworks/HWL9/task3.py
@@ -0,0 +1,22 @@
+class MyMetaClass(type):
+    obj = None
+
+    def __new__(mcs, clsname, bases, clsdict):
+        return super(MyMetaClass, mcs).__new__(mcs, clsname, bases, clsdict)
+
+    def __init__(cls, clsname, bases, clsdict):
+        super(MyMetaClass, cls).__init__(clsname, bases, clsdict)
+
+    def __call__(cls, *args, **kwargs):
+        if cls.obj is None:
+            cls.obj = super(MyMetaClass, cls).__call__(*args, **kwargs)
+        return cls.obj
+
+
+class MyClass(metaclass=MyMetaClass):
+    pass
+
+
+obj_1 = MyClass()
+obj_2 = MyClass()
+print(obj_1 is obj_2)
