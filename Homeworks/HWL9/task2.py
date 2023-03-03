54de32ed12be3c33d53fde313955e7342a62e678
Evgenii Solovev <krekersol@mail.ru>
2023-03-03 19:38:39 +0900

    Удалены неиспользуемые get и delete в задании 2 семинара 9

--- Homeworks/HWL9/task2.py
+++ Homeworks/HWL9/task2.py
@@ -24,16 +24,10 @@ class NonNegative:
     def __set_name__(self, owner, my_attr):
         self.my_attr = my_attr
 
-    def __get__(self, instance, owner):
-        return instance.__dict__[self.my_attr]
-
     def __set__(self, instance, value):
         if value < 0:
             raise ValueError("не может быть отрицательным!")
         instance.__dict__[self.my_attr] = value
-
-    def __delete__(self, instance):
-        del instance.__dict__[self.my_attr]
 
 
 class Road:
